41c90372357d75dabd545b547442750e0d429845
David Ma <davidma415@gmail.com>
2018-07-17 10:12:02 -0400

    Restructured to put everything inside functions

--- exercise8.rb
+++ exercise8.rb
@@ -26,26 +26,27 @@ def print_states(p1, p2, p3)
   end
 end
 
+def init
+  puts "How far did person 1 run (in metres)?"
+  distance1 = gets.to_f
+  puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
+  mins1 = gets.to_f
 
-puts "How far did person 1 run (in metres)?"
-distance1 = gets.to_f
-puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
-mins1 = gets.to_f
+  puts "How far did person 2 run (in metres)?"
+  distance2 = gets.to_f
+  puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
+  mins2 = gets.to_f
 
+  puts "How far did person 3 run (in metres)?"
+  distance3 = gets.to_f
+  puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
+  mins3 = gets.to_f
 
-puts "How far did person 2 run (in metres)?"
-distance2 = gets.to_f
-puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
-mins2 = gets.to_f
+  p1 = person(distance1, mins1)
+  p2 = person(distance2, mins2)
+  p3 = person(distance3, mins3)
 
+  print_states(p1, p2, p3)
+end
 
-puts "How far did person 3 run (in metres)?"
-distance3 = gets.to_f
-puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
-mins3 = gets.to_f
-
-p1 = person(distance1, mins1)
-p2 = person(distance2, mins2)
-p3 = person(distance3, mins3)
-
-print_states(p1, p2, p3)
+init
