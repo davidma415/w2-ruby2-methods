ac45e18d9c6a52d066957aa2139418e507914f66
David Ma <davidma415@gmail.com>
2018-07-17 00:22:29 -0400

    Changed structure

--- exercise8.rb
+++ exercise8.rb
@@ -12,6 +12,21 @@ def person (dist, mins)
   return secs, speed
 end
 
+def print_states(p1, p2, p3)
+  if p3[1] > p2[1] && p3[1] > p1[1]
+    puts "Person 3 was the fastest at #{p3[1]} m/s"
+  elsif p2[1] > p3[1] && p2[1] > p1[1]
+    puts "Person 2 was the fastest at #{p2[1]} m/s"
+  elsif p1[1] > p3[1] && p1[1] > p2[1]
+    puts "Person 1 was the fastest at #{p1[1]} m/s"
+  elsif p1[1] == p2[1] && p2[1] == p3[1]
+    puts "Everyone tied at #{p1[1]} m/s"
+  else
+    puts "Well done everyone!"
+  end
+end
+
+
 puts "How far did person 1 run (in metres)?"
 distance1 = gets.to_f
 puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
@@ -33,14 +48,4 @@ p1 = person(distance1, mins1)
 p2 = person(distance2, mins2)
 p3 = person(distance3, mins3)
 
-if p3[1] > p2[1] && p3[1] > p1[1]
-  puts "Person 3 was the fastest at #{p3[1]} m/s"
-elsif p2[1] > p3[1] && p2[1] > p1[1]
-  puts "Person 2 was the fastest at #{p2[1]} m/s"
-elsif p1[1] > p3[1] && p1[1] > p2[1]
-  puts "Person 1 was the fastest at #{p1[1]} m/s"
-elsif p1[1] == p2[1] && p2[1] == p3[1]
-  puts "Everyone tied at #{p1[1]} m/s"
-else
-  puts "Well done everyone!"
-end
+print_states(p1, p2, p3)
